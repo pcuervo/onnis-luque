5f3dbe336485ca70b44dfcd7430e71463deeadc0
Arisbeth <aris@pcuervo.com>
2016-09-21 16:06:16 -0500

    talleres-noticias, box-hover :publicaciones

--- themes/onnis-luque/archive-editorial.php
+++ themes/onnis-luque/archive-editorial.php
@@ -15,10 +15,13 @@ $postCounter = 0;
 				while ( have_posts() ) : the_post();
 				?>
 					<div class="[ column xmall-6 medium-4 large-3 ][ margin-bottom ]">
-						<a class="[ block ][ js-editorial-cover ]" href="#" data-number="<?php echo $postCounter; ?>">
+						<a class="[ block ][ relative ][ box-hover-content ]" href="#" data-number="<?php echo $postCounter; ?>">
 							<?php the_post_thumbnail('single_editorial', array('class' => '[ image-responsive ]')); ?>
+							<div class="[ absolute ][ text-center ][ color-dark ]">
+								<h3 class="[ uppercase ]"><?php echo get_the_title(); ?></h3>
+								<p><?php the_content(); ?></p>
+							</div>
 						</a>
-
 					</div>
 				<?php $postCounter++; endwhile; ?>
 			</div>
@@ -26,51 +29,9 @@ $postCounter = 0;
 	</section><!-- editorial -->
 <?php endif; wp_reset_query(); ?>
 
-
-
-
 <!-- =================================================
 ==== MODAL EDITORIAL
 ================================================== -->
-<section class="[ modal-wrapper modal-editorial ][ hide ]">
-	<div class="[ modal modal--full ]">
-		<div class="[ xmall-12 ][ close-modal ][ clearfix ][ absolute z-index-6 ]">
-			<p class="[ text-center ]"><a class="[ block ][ padding ][[ bg-transparent ][ js-gallery-toggler ]" data-modal="contacto" href="#">
-				Cerrar
-			</a></p>
-		</div>
-		<div class="[ modal-content ][ wrapper ][ text-center ]">
-			<div class="[ slideshow ]">
-				<?php
-				$postCounter = 1;
-				if( have_posts() ) : while ( have_posts() ) : the_post();
 
-					$galleries = get_galleries_from_content($post);
-					foreach ($galleries as $gallery => $galleryIDs) {
-						$images = sga_gallery_images('full', $galleryIDs); ?>
-						<div class="[ row ]">
-							<?php
-							foreach ($images as $key => $image) {
-								$imageID 	= $image[4];
-								$imageURL	= $image[0];
-							?>
-								<div class="[ image-single ][ bg-light ]">
-									<p class="[ clearfix ][ cycle-info ][ text-center ]"><?php echo $postCounter; ?>/<?php echo $wp_query->post_count;; ?></p>
-									<img class="[ center block ]" src="<?php echo $imageURL; ?>">
-								</div>
-							<?php
-							} ?>
-						</div><!-- row -->
-					<?php
-					}
-
-				$postCounter++; endwhile; endif; ?>
-
-				<p class="[ span xmall-6 ][ inline-block align-middle ][ text-left ][ no-margin ][ cycle-control cycle-prev ]">Atras</p><p
-					class="[ span xmall-6 ][ inline-block align-middle ][ text-right ][ cycle-control cycle-next ]">Siguiente</p>
-			</div><!-- slideshow -->
-		</div>
-	</div><!-- modal-content -->
-</section>
 
 <?php get_footer(); ?>
